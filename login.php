85195dcb0aa1a26bd5dd06ed0622214af29a1dfc
Sol-ha <101246194+Sol-Ha@users.noreply.github.com>
2022-06-11 18:43:22 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,7 +1,44 @@
+<!-- this is where magic happens for your security check works
+it will pick the right files where all orders are displayed and
+interrogating your database -->
+<?php require("actions/loginAction.php"); ?>
 <!DOCTYPE html>
-<html lang="en">
-<?php include "includes/head.php";?>
+<html lang="fr">
+
+<!-- reduce space by picking the corresponding file in "includes" -->
+<?php include "includes/head.php"; ?>
+
 <body>
-    
+
+    <!-- reduce space by picking the corresponding file in "includes" -->
+    <?php include "includes/header.php"; ?>
+
+
+    <main class="main_loginANDsignup">
+        <div class="line"></div>
+        <form>
+            <h1>Connection</h1>
+
+            <!-- pick the error message from the right action file -->
+            <?php if (isset($errorMsg)) {
+                echo "<p>" . $errorMsg . "</p>";
+            } ?>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Pseudo</label>
+                <input type="email" class="form-control" name="pseudo">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            <button type="submit" class="btn" name="validate">Se connecter</button>
+        </form>
+    </main>
+
+    <footer>
+        <h2>Vous n'avez pas de compte?</h2>
+        <a href="signup.php">Cliquez ici</a>
+    </footer>
 </body>
+
 </html>
--- signup.php
+++ signup.php
@@ -1,7 +1,18 @@
+<!-- this is where magic happens for your security check works
+it will pick the right files where all orders are displayed and
+interrogating your database -->
+<?php require ("actions/signupAction.php");?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
+<!-- reduce space by picking the corresponding file in "includes" -->
 <?php include "includes/head.php";?>
 <body>
-    
+
+<!-- reduce space by picking the corresponding file in "includes" -->
+<?php include "includes/header.php";?>
+
+<!-- pick the error message from the right action file -->
+<?php if(isset($errorMsg)) {echo "<p>".$errorMsg."</p>";}?>
 </body>
 </html>
